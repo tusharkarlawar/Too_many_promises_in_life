abc1000e0a9f4ea09130192b80d9afeee68faf3d
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-06-02 15:05:13 +0530

    TooManyPromises

--- promises.js
+++ promises.js
@@ -1,46 +1,58 @@
 const posts = [];
-let lastActivityTime = null;
 
-function createPost(post) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
+      function createPost(post) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
             posts.push(post);
-            resolve();
-        }, 1000);
-    });
-}
 
-function updateLastUserActivityTime() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            lastActivityTime = new Date();
             resolve();
-        }, 1000);
-    });
-}
+          }, 1000);
+        });
+      }
 
-function deletePost() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
+      function updateLastUserActivityTime() {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const lastActivityTime = new Date().toLocaleString();
+
+            resolve(lastActivityTime);
+          }, 1000);
+        });
+      }
+
+      function deleteLastPost() {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
             if (posts.length > 0) {
-                const deletedPost = posts.pop();
-                resolve(deletedPost);
-            } else {
-                reject("ERROR: NO POST FOUND");
-            }
-        }, 1000);
-    });
-}
+              const deletedPost = posts.pop();
 
-createPost({ title: 'POST1' })
-    .then(() => updateLastUserActivityTime())
-    .then(() => {
-        console.log('Posts:', posts);
-        console.log('Last Activity Time:', lastActivityTime);
-        return deletePost();
-    })
-    .then(deletedPost => {
-        console.log('Deleted Post:', deletedPost);
-        console.log('New Posts:', posts);
-    })
-    .catch(error => console.error(error));
+              resolve(deletedPost);
+            } else {
+              reject("ERROR: No posts to delete");
+            }
+          }, 1000);
+        });
+      }
+
+      // Call the functions and handle the promises to get the desired output
+
+      createPost({ title: "Post 1" })
+        .then(() => updateLastUserActivityTime())
+
+        .then((lastActivityTime) => {
+          console.log("Posts:", posts);
+
+          console.log("Last Activity Time:", lastActivityTime);
+
+          return deleteLastPost();
+        })
+
+        .then((deletedPost) => {
+          console.log("Deleted Post:", deletedPost);
+
+          console.log("New Posts:", posts);
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
